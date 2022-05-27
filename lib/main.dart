cad96537c70f364333f4425a060331fb3d76c1a3
vinu.gilbert <vinu.gilbert@zcocorporation.com>
2022-05-27 16:56:21 +0530

    email and password validation

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,46 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  String emailPattern =
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+      r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+      r"{0,253}[a-zA-Z0-9])?)*$";
+  String passwordPattern =
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+
+  void _checkTextFieldValidation(BuildContext context) {
+    String useremail, password;
+    useremail = emailController.text;
+    password = passwordController.text;
+    if (useremail == '' || password == '') {
+      _showSnackBar(context, 'Fields are empty');
+    } else {
+      if (useremail != '' && !RegExp(emailPattern).hasMatch(useremail)) {
+        _showSnackBar(context, 'Enter Valid email Address');
+      } else {
+        if (password != '' && !RegExp(passwordPattern).hasMatch(password)) {
+          _showSnackBar(context, 'Enter Valid Password');
+        }
+        else{
+          _showSnackBar(context, 'All Good');
+        }
+      }
+    }
+  }
+
+  _showSnackBar(BuildContext context, String validationInfo) {
+    final snackBar = SnackBar(
+      content: Text(validationInfo),
+      backgroundColor: (Colors.black12),
+      action: SnackBarAction(
+        label: 'dismiss',
+        onPressed: () {},
+      ),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,10 +94,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             Container(
               padding: const EdgeInsets.all(10),
               child: TextField(
-                controller: nameController,
+                controller: emailController,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'User Name',
+                  labelText: 'Email Address',
                 ),
               ),
             ),
@@ -78,7 +116,9 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               onPressed: () {
                 //forgot password screen
               },
-              child: const Text('Forgot Password',),
+              child: const Text(
+                'Forgot Password',
+              ),
             ),
             Container(
                 height: 50,
@@ -86,18 +126,16 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 child: ElevatedButton(
                   child: const Text('Login'),
                   onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
+                    _checkTextFieldValidation(context);
                   },
-                )
-            ),
+                )),
             Row(
               children: <Widget>[
                 const Text('Does not have account?'),
                 TextButton(
                   child: const Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
+                    'Sign In',
+                    style: TextStyle(fontSize: 20, color: Colors.deepOrange),
                   ),
                   onPressed: () {
                     //signup screen
